6951c839fbef68926e8caa960bc7971dafa14b4f
Ryuzo911 <runfromback911gmail.com>
2024-08-25 06:36:11 +0800

    add link

--- app/page.jsx
+++ app/page.jsx
@@ -5,6 +5,7 @@ import Social from "@/components/Social";
 import Stats from "@/components/Stats";
 import { Button } from "@/components/ui/button"
 import { FiDownload } from "react-icons/fi"
+import { Link } from "next/link";
 
 const Home = () => {
   return (
@@ -20,10 +21,12 @@ const Home = () => {
                 Gue adalah software engineer yang mendalami bidang backend dan gue juga berprofesi sebagai barista part time di salah satu coffee shop.
               </p>
               <div className="flex flex-col xl:flex-row items-center gap-8">
-                <Button variant="outline" size="lg" className="uppercase flex items-center gap-2" href="/assets/CV_Muhammad_Rizal.pdf" passHref>
+                <Link href="/assets/CV_Muhammad_Rizal.pdf" passHref>
+                <Button variant="outline" size="lg" className="uppercase flex items-center gap-2" >
                   <span>Download CV</span>
                   <FiDownload className="text-xl"/>
                 </Button>
+                </Link>
                 <div className="mb-8 xl:mb-0 ">
                   <Social containerStyles="flex gap-6" iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"/>
                 </div>
